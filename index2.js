424fe04fb99d9a64a58dc660af9b89cbc35a0f53
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2023-09-07 01:22:39 +0530

    add

--- index2.js
+++ index2.js
@@ -5,6 +5,10 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(
     import.meta.url));
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -25,7 +29,7 @@ app.get("/", (req, res) => {
         response.on("end", () => {
             try {
                 const result = JSON.parse(data);
-                res.render("./index.ejs", { activity: result.activity });
+                res.render("./views/index.ejs", { activity: result.activity });
             } catch (error) {
                 console.error("Failed to parse response", error.message);
                 res.status(500).send("Failed to send Activity. Please try again");
@@ -40,6 +44,28 @@ app.get("/", (req, res) => {
 
     request.end();
 });
+
+app.post("/", async(req, res) => {
+    try {
+        console.log(req.body);
+        const type = req.body.type;
+        const participants = req.body.participants;
+        const response = await axios.get(
+            `https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`
+        );
+        const result = response.data;
+        console.log(result);
+        res.render("index.ejs", {
+            data: result[Math.floor(Math.random() * result.length)],
+        });
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+            error: "No activities that match your criteria.",
+        });
+    }
+});
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
